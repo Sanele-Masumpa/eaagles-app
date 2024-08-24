af8ee5bfddae9915d3977456bdb5ab0d7f6755f2
Sanele-Masumpa <masumpasanele5@gmail.com>
2024-08-24 15:40:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Configure body parser settings for API routes
+  api: {
+    bodyParser: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
