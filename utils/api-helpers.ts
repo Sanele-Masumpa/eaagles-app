5d331c2dddd5f1e0ba3bf3d16b6b28edd6734e01
Sanele-Masumpa <masumpasanele5@gmail.com>
2024-08-24 17:09:52 +0200

    Update api-helpers.ts

--- utils/api-helpers.ts
+++ utils/api-helpers.ts
@@ -3,28 +3,36 @@ export async function fetchGetJSON(url: string) {
     const data = await fetch(url).then((res) => res.json());
     return data;
   } catch (err) {
-    throw new Error(err.message);
+    // Assert err as Error type
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error('An unknown error occurred');
+    }
   }
 }
 
 export async function fetchPostJSON(url: string, data?: {}) {
   try {
-    // Default options are marked with *
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *client
-      body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data || {}),
     });
-    return await response.json(); // parses JSON response into native JavaScript objects
+    return await response.json();
   } catch (err) {
-    throw new Error(err.message);
+    // Assert err as Error type
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error('An unknown error occurred');
+    }
   }
 }
